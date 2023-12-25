d95bbd295accce5a622406a8d09eccb89f780797
SOUN NIVOTH <109658258+Nivoth@users.noreply.github.com>
2023-12-25 16:44:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+//alert("Update website ")
+const menu= document.getElementById("menu");
+const menu_icon=document.getElementById("menu_icon");
+
+menu_icon.addEventListener("click", ()=> {
+    menu.classList.toggle("toggleMenu");
+})
